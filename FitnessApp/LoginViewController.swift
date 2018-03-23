e10b6c1310185cb79365d46c48b4595636798e73
xhpwn <saxena20@purdue.edu>
2018-03-22 22:30:13 -0400

    Working Firebase

--- FitnessApp/LoginViewController.swift
+++ FitnessApp/LoginViewController.swift
@@ -13,9 +13,36 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var headLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loginButton.layer.cornerRadius = 10
+        loginButton.clipsToBounds = true
+        
+        UIView.animate(withDuration: 1.0) {
+            self.headLabel.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+            
+        }
+        
+        UIView.animate(withDuration: 2.0) {
+            self.headLabel.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            
+        }
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseOut],
+                       animations: {
+                        self.headLabel.center.y -= self.view.bounds.height - 100
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.5) { () -> Void in
+            self.headLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.45, options: UIViewAnimationOptions.curveEaseIn, animations: { () -> Void in
+            self.headLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi * 2.0)
+        }, completion: nil)
+        
         // Do any additional setup after loading the view.
     }
 
@@ -56,7 +83,7 @@ class LoginViewController: UIViewController {
     @IBAction func didSignup(_ sender: Any) {
         
         if usernameField.text == "" || passwordField.text == "" {
-            let alert = UIAlertController(title: "Failed!" , message: "Check for empty field", preferredStyle: UIAlertControllerStyle.alert)
+            let alert = UIAlertController(title: "Failed!" , message: "No password or username", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
         }
